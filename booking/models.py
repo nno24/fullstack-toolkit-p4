ff2d41def7b2a4779decb24f63d800e892a6dcc1
nno24 <nicolai.norseng@gmail.com>
2022-02-04 23:32:04 +0000

    some changes on the form handling, but unable to get the POST data correctly

--- booking/models.py
+++ booking/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
+
+class Booking(models.Model):
+    name = models.CharField(max_length=10, null=False, blank=False)
+    email = models.CharField(max_length=50, null=False, blank=False)
+
+    def __str__(self):
+        return f'{self.name} - {self.email}'
--- booking/views.py
+++ booking/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Booking
+from .forms import BookingForm
+
 
 # Create your views here.
 
@@ -8,8 +11,13 @@ def get_home(request):
 
 
 def get_booking(request):
+    if request.method == 'POST':
+        print('the request is: ', request.POST.get('item_name'))
+        return redirect('get_home')
     return render(request, 'booking/book.html')
 
 def back_home(request):
     return render(request, 'booking/home.html')
+
+ 
 
