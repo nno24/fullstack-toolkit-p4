82149b35a5bed731400a4422833cc120c66ce540
nno24 <nicolai.norseng@gmail.com>
2022-02-07 08:25:41 +0000

    Allow from one site

--- resturant/settings.py
+++ resturant/settings.py
@@ -30,7 +30,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-#X_FRAME_OPTIONS = 'SAMEORIGIN'  
+#X_FRAME_OPTIONS = 'SAMEORIGIN'
+X_FRAME_OPTIONS = 'ALLOW-FROM http://ami.responsivedesign.is/'
 
 ALLOWED_HOSTS = ['resturant-booking-p4.herokuapp.com', 'localhost']
 
