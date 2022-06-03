4a4e06ca14081df0e82a052a0b0051cdcfd82db8
nno24 <nicolai.norseng@gmail.com>
2022-06-03 16:36:30 +0000

    Change in from email

--- resturant/settings.py
+++ resturant/settings.py
@@ -219,7 +219,7 @@ SOCIALACCOUNT_PROVIDERS = {
 # Emails ---------------------
 if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-    DEFAULT_FROM_EMAIL = 'nikolay.cranner@mail.com'
+    DEFAULT_FROM_EMAIL = 'nikolay.cranner@gmail.com'
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_USE_TLS = True
